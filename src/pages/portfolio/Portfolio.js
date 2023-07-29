29221b8b8170d744df6c0ba2175011bb44a673a9
Mokni Hamdi <hamdimokni712@gmail.com>
2023-07-29 14:11:22 +0100

    update

--- src/pages/portfolio/Portfolio.js
+++ src/pages/portfolio/Portfolio.js
@@ -20,7 +20,7 @@ const Portfolio = () => {
 
   return (
     <section className="portfolio">
-      <PageHeader title="Portfolio" description="View my work" />
+      <PageHeader title="Portfolio" description="View mys work" />
       <div className="row">
         <ProjectList />
       </div>
