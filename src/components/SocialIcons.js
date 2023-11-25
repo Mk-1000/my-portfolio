38caabb0c25a6d71f46e95ed8a90d7eac4efe91a
Mokni Hamdi <86926622+Mk-1000@users.noreply.github.com>
2023-11-25 11:13:58 +0100

    solving warnnings from _blank

--- src/components/SocialIcons.js
+++ src/components/SocialIcons.js
@@ -10,10 +10,10 @@ const SocialIcons = () => {
 
   return (
     <div className="socialIcons" style={styles.socialIcons}>
-      <a className="icon" style={styles.icon} href="https://github.com/Mk-1000" target="_blank">
+      <a className="icon" style={styles.icon} href="https://github.com/Mk-1000" target="_blank" rel="noreferrer">
         <i className="fa-brands fa-github" aria-hidden="true" title="Mokni hamdi' GitHub Profile"></i>
       </a>
-      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/mokni-hamdi-mk712/" target="_blank">
+      <a className="icon" style={styles.icon} href="https://www.linkedin.com/in/mokni-hamdi-mk712/" target="_blank" rel="noreferrer">
         <i className="fa-brands fa-linkedin" aria-hidden="true" title="Mokni hamdi' LinkedIn Profile" ></i>
       </a>
     </div>
